a7c58bea5e2f7853cf9356b667411ab3fb7b52bb
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-11-15 19:30:11 +0100

    Add userSlice

--- src/types/types.ts
+++ src/types/types.ts
@@ -11,15 +11,33 @@ export interface UserRegisterData {
   picture?: string;
 }
 
+export interface Credentials {
+  username: string;
+  password: string;
+}
+
 export interface UsersState {
   list: User[];
 }
 
 export interface User {
-  name: string;
+  username: string;
   email: string;
   picture: string;
   id: string;
   friends: [];
   enemies: [];
+}
+
+export interface UserLoginData {
+  id: string;
+  token: string;
+  username: string;
+}
+
+export interface UserState {
+  username: string;
+  id: string;
+  token: string;
+  isLogged: boolean;
 }
